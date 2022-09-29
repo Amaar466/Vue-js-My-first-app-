696018adcd0709bb73a777dc47253c047431fd47
amaar <amaarulhaq39@gmail.com>
2022-09-28 23:59:53 -0700

    Interpolation data

--- src/components/Photo.vue
+++ src/components/Photo.vue
@@ -1,5 +1,17 @@
 <template>
-    <h1>Photo Component</h1>
+    <!-- interpolation in Vue Js  -->
+    <!-- {{}} -->
+    <!-- check lenght  -->
+    <h1>Photo Component {{"cricket".length}}</h1>
+     <!-- use function with interpolation / parameter -->
+     <h1 style="color:orange;">Company: {{getName('Conzummate')}}</h1>
+    <!-- return any thing fron script to html  -->
+    <h2>Email:  {{email}}</h2>
+    <h1>Mobile:  {{mobile}}</h1>
+    <h1>Mobile:  {{mobile==8888}}</h1>
+   
+    
+
     
 </template>
 
@@ -7,6 +19,20 @@
 
     export default {
         Name:'Photo',
+        data()
+        {
+            return {
+                email:'amaar@gmail.com',
+                mobile:34885945,
+                getName:function(a){
+                    // return 'Conzummate Tech'
+                      return a;
+                
+                }
+               
+
+            }
+        }
     }
 </script>
 <style scoped>
