9fe419c1211db42431ef122a745073369bce3f1f
Amaar466 <amaarulhaq39@gmail.com>
2023-01-17 13:42:09 +0500

    Reuse Component and user component

--- src/components/Reuse.vue
+++ src/components/Reuse.vue
@@ -1,14 +1,38 @@
 <template>
     <h1>Reuse Components</h1>
-    <User />
+  <ul>
+    <li v-for=" item in users" :key="item.name">
+
+        <User :data="item" :getData="getData"/>
+        <!-- {{ item.name }}
+        {{ item.email }} -->
+    </li>
+  </ul>
+    
 </template>
 
 <script>
 import User from './User.vue'
-export default{
-    Name:'Reuse',
-    components:{
+export default {
+    Name: 'Reuse',
+    components: {
         User
+    },
+    methods:{
+        getData(name)
+        {
+            alert(name);
+        }
+    },
+    data() {
+        return {
+       users:[
+            {name: 'amaar', email: 'amaar@gmail.com' },
+            { name: 'bilal', email: 'bilal@gmail.com' },
+            { name: 'hadi', email: 'hadi@gmail.com' },
+            { name: 'ali', email: 'ali@gmail.com' },
+        ],
+            }
     }
 }
 </script>
--- src/components/User.vue
+++ src/components/User.vue
@@ -1,9 +1,28 @@
 <template>
-<h1>User Components</h1>
+
+<div class="user">    
+<h2>{{data.name}}</h2>
+<h2>{{data.email}}</h2>
+<button v-on:click="getData(data.name)">Alert Name</button>
+</div>
 </template>
 
 <script>
 export default{
     Name:'User',
+    props:{
+        data:Object,
+        getData:Function
+    }
 }
 </script>
+
+<style>
+.user{
+    background-color: skyblue;
+    padding: 10px;
+    border-bottom: 1px solid;
+    margin-bottom: 20px;
+}
+
+</style>
